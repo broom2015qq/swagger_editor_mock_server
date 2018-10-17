57ad1698008ad6daa4544ac2ebb7dd2f154626a1
broom2015qq <897500516@qq.com>
2018-10-17 16:24:00 +0800

    map type swagger

--- src/main/java/com/chinaunicom/controller/UserController.java
+++ src/main/java/com/chinaunicom/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.chinaunicom.controller;
 
 import com.chinaunicom.Service.*;
+import com.chinaunicom.controller.agent.annotation.ApiJsonObject;
+import com.chinaunicom.controller.agent.annotation.ApiJsonProperty;
 import com.chinaunicom.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
+import java.util.Map;
 
 @RestController
 @Validated
@@ -22,9 +23,21 @@ public class UserController {
     @RequestMapping(value="/hello",method = RequestMethod.GET )
 //    用在方法上，说明方法的作用
     @ApiOperation(value = "查询个人信息接口",notes = "查询个人信息接口")
-
     public User say(@RequestParam("id") int id){
         return UserService.selectById(id);
     }
+
+    @RequestMapping(value="/map_post",method = RequestMethod.POST )
+    //    map类型的body
+    @ApiOperation(value = "登陆", tags = "登陆")
+    public void auth(@ApiJsonObject(name = "login_model", value = {
+        @ApiJsonProperty(key = "mobile", example = "18614242538", description = "user mobile"),
+        @ApiJsonProperty(key = "password", example = "123456", description = "user password")
+})
+        @RequestBody Map<String, String> params) {
+            System.out.println("map类型");
+            System.out.println("mobile:"+params.get("mobile"));
+            System.out.println("password:"+params.get("password"));
+        }
 }
 
